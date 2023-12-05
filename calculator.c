35d4fe3133ec89c71e0702f38ad0ed012e206724
ali_mansourian <alimansourian1383@gmail.com>
2023-12-05 03:35:57 +0330

    changed calculate in cherry pick

--- calculator.c
+++ calculator.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
+int pow2(int a) {
+ 
+ return a * a;}
+ 
 int calculate(int a, int b) {
- // A different calculation
-<<<<<<< HEAD
- return a * b * 2 -10 ;}
-=======
- return a + b;}
->>>>>>> feature-conflict
+ // A combination of both calculations
+ return a * b * 2 -10 + (a + b);}
